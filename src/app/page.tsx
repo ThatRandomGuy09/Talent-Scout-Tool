66ccde5b2573f19451ee42906809c358f4e4664d
Vansh Vasishtha <vansh19092002@gmail.com>
2024-04-04 19:08:02 +0530

    Header and Loader added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,22 @@
+import { Button } from "@/components/ui/button";
 import { db } from "@/db";
+import Link from "next/link";
 import React from "react";
 
 export default async function Home() {
   const rooms = await db.query.room.findMany();
   return (
-    <div>
+    <main className="min-h-screen p-16">
+      <div className="flex justify-between items-center">
+        <h1 className="text-4xl">Find Dev Rooms</h1>
+        <Button asChild>
+          <Link href="/create-room">Create Room</Link>
+        </Button>
+      </div>
+
       {rooms.map((room) => {
         return <div key={room.name}>{room.name}</div>;
       })}
-    </div>
+    </main>
   );
 }
