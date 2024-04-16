8258bd1b596935cf60c8cb35cc92c5192468891f
Vansh Vasishtha <vansh19092002@gmail.com>
2024-04-16 22:12:29 +0530

    Your rooms Updated

--- src/app/your-rooms/page.tsx
+++ src/app/your-rooms/page.tsx
@@ -13,9 +13,6 @@ export default async function YourRoomsPage() {
     <main className="min-h-screen p-16">
       <div className="flex justify-between items-center mb-8">
         <h1 className="text-4xl">Your Rooms</h1>
-        <Button asChild>
-          <Link href="/create-room">Create Room</Link>
-        </Button>
       </div>
 
       <div className="grid grid-cols-3 gap-4">
@@ -26,12 +23,7 @@ export default async function YourRoomsPage() {
 
       {rooms.length === 0 && (
         <div className="flex flex-col gap-4 justify-center items-center mt-24">
-          <Image
-            src="/no-data.svg"
-            width="200"
-            height="200"
-            alt="no data image"
-          />
+          <Image src="/pnf.svg" width="200" height="200" alt="no data image" />
 
           <h2 className="text-2xl">You have no rooms</h2>
 
