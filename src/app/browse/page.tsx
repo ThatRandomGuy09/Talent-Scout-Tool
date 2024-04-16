274fb2c48617a4bcb374b5663091e43b9727941c
Vansh Vasishtha <vansh19092002@gmail.com>
2024-04-16 22:12:00 +0530

    Browse page final done

--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -4,6 +4,7 @@ import { getRooms } from "@/data-access/rooms";
 import { SearchBar } from "./search-bar";
 import { RoomCard } from "@/app/browse/room-card";
 import { unstable_noStore } from "next/cache";
+import Image from "next/image";
 
 export default async function Home({
   searchParams,
@@ -32,6 +33,13 @@ export default async function Home({
           return <RoomCard key={room.id} room={room} />;
         })}
       </div>
+      {rooms.length === 0 && (
+        <div className="flex flex-col gap-4 justify-center items-center mt-24">
+          <Image src="/pnf.svg" width="200" height="200" alt="no data image" />
+
+          <h2 className="text-2xl">No Rooms Yet!</h2>
+        </div>
+      )}
     </main>
   );
 }
